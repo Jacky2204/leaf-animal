76a39987bab0ddb287267103a37f0d42d3fd2fe6
Jacky2204 <jalindarbhapkar2@gmail.com>
2024-05-25 16:55:02 +0530

    conflict

---
+++ src/component/How.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "./How.css";
+
+export function Main() {
+  return (
+    <>
+      <div  >
+        <div className='box'>
+            
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/view/How.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Main } from './../component/How.js';
+
+export function How(){
+<>
+    <Main />
+  </>
+}
+  
+
