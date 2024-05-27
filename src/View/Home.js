72f219f9ede66f83e639f191603ad0b12b1d5963
kiranmadage <kiranmadage96@gmail.com>
2024-05-27 22:35:09 +0530

    first page

--- src/View/Home.js
+++ src/View/Home.js
@@ -1,9 +1,18 @@
-import {Navbar} from "./../Component/Navbar"
+import {Navbar} from "./../Component/Home"
+import {Img} from "./../Component/Home"
+import {Text} from "./../Component/Home"
+import {Text1} from "./../Component/Home"
+import {Footer} from "./../Component/Home"
 
 export function Home(){
     return(
         <>
         <Navbar/>
+        <Img/> 
+        <Text/> 
+        <Text1/>
+        <Footer/>
+        
         </>
     )
-}
+} 
